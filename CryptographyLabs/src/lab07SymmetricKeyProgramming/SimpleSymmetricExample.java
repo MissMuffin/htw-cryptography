8ce2f94f7cabd06ac42cc0a488a8f091552b9880
MissMuffin <fyyree@gmail.com>
2015-12-18 17:59:22 +0100

    added md5 implementation

--- CryptographyLabs/src/lab07SymmetricKeyProgramming/SimpleSymmetricExample.java
+++ CryptographyLabs/src/lab07SymmetricKeyProgramming/SimpleSymmetricExample.java
@@ -6,8 +6,10 @@ import java.security.spec.KeySpec;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
@@ -16,34 +18,50 @@ public class SimpleSymmetricExample {
 
 		Security.addProvider(new BouncyCastleProvider());
 
-		String keyString = "B7c332yXza";
-		String messageString = "hallowelt";
+		String keyString = "12c4U05:)a201i5iya2A";
+		String messageString = "And the band begins to play... We all live in a yellow submarine yellow submarine";
 
 		byte[] keyBytes = keyString.getBytes(Charset.forName("UTF-8"));
 		System.out.println(keyBytes.length);
 		byte[] messageBytes = messageString.getBytes(Charset.forName("UTF-8"));
 
-		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1", "BC");
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 		KeySpec spec = new PBEKeySpec(keyString.toCharArray(), "123456789".getBytes(), 4096, 128);
 		SecretKey tmp = factory.generateSecret(spec);
 		SecretKey key = new SecretKeySpec(tmp.getEncoded(), "AES");
 
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC");
 
 		System.out.println("input text : " + messageString);
 
 		// encryption pass
 		cipher.init(Cipher.ENCRYPT_MODE, key);
 		byte[] ciphertext = cipher.doFinal(messageBytes);
-		// byte[] iv = cipher.getIV();
+		byte[] iv = cipher.getIV();
+		System.out.println("iv :" + Utils.toHex(iv));
 		System.out.println("cipher text: " + Utils.toHex(ciphertext) + " bytes: " + ciphertext.length);
 
 		// decryption pass
-		cipher.init(Cipher.DECRYPT_MODE, key);
-		byte[] plaintext = cipher.doFinal(ciphertext);// removed offset of
-														// ctlength
-
-		System.out.println("plain text : " + Utils.toHex(plaintext) + " bytes: " + plaintext.length);
-		System.out.println(new String(plaintext));
+		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+		
+//		String bla = "a2838036d47ec016d112916f10ff21b2e6da27c68e8a3f64835e08f2f4290fa8";
+//		byte[] test = DatatypeConverter.parseHexBinary(bla);
+//		
+//		byte[] plaintext = cipher.doFinal(test);
+		byte[] plaintext = cipher.doFinal(ciphertext);
+		
+		System.out.println("plain text : " + new String(plaintext) + " bytes: " + plaintext.length);
 	}
+	
+	
+	/**
+	 * ENCRYPT
+	 * Esteban			A3b472xAbc
+	 * Julien			x752jU5ie2
+	 * Dorette			1234567
+	 * Lucy&Charlie		x3y45uCiC451y!?36
+	 * Micha&Felix		password
+	 * Solveig			drowssap
+	 * Ruslan&Stiliyana 12c4U05:)a201i5iya2A
+	 */
 }
--- CryptographyLabs/src/lab07SymmetricKeyProgramming/SymmetricWithMD5.java
+++ CryptographyLabs/src/lab07SymmetricKeyProgramming/SymmetricWithMD5.java
@@ -16,12 +16,18 @@ public class SymmetricWithMD5 {
 		
 		Security.addProvider(new BouncyCastleProvider());
 		
-		String messageString = "12345678";
+//		String messageString = "In the town where I was born Lived a man who sailed to sea";
+//		String messageString = "And he told us of his life In the land of submarines";
+//		String messageString = "So we sailed on to the sun Till we found a sea of green";
+//		String messageString = "And we lived beneath the waves In our yellow submarine";
+//		String messageString = "We all live in a yellow submarine Yellow submarine yellow submarine";
+//		String messageString = "And our friends are all aboard Many more of them live next door";
+		String messageString = "And the band begins to play... We all live in a yellow submarine yellow submarine";
 		byte[] message = messageString.getBytes(Charset.forName("UTF-8"));
-		byte[] paddedMessage = new byte[message.length + 8 - (message.length % 8)];
+		byte[] paddedMessage = new byte[message.length];
 		System.arraycopy(message, 0, paddedMessage, 0, message.length);
 		
-		String keyString = "DODODELWoD3LBOOGieMoGY";			
+		String keyString = "12c4U05:)a201i5iya2A";			
 		byte[] keyBytes = keyString.getBytes(Charset.forName("UTF-8"));
 
 		MessageDigest md = MessageDigest.getInstance("MD5");
@@ -29,30 +35,47 @@ public class SymmetricWithMD5 {
 		byte[] hashedKey = md.digest();
 		
 		SecretKeySpec key = new SecretKeySpec(hashedKey, "AES");
-		Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC");
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
 		System.out.println(new String(message));
-		System.out.println("input text : " + Utils.toHex(paddedMessage));
+		System.out.println("input text : " + Utils.toHex(paddedMessage) + " bytes: " + paddedMessage.length);
 
 		// encryption pass
-
 		cipher.init(Cipher.ENCRYPT_MODE, key);
 		byte[] cipherText = cipher.doFinal(paddedMessage);		
 		System.out.println("cipher text: " + Utils.toHex(cipherText) + " bytes: " + cipherText.length);
 
 		// decryption pass
-
 		cipher.init(Cipher.DECRYPT_MODE, key);
-//		byte[] plainText = cipher.doFinal(cipherText);
+		byte[] plainText = cipher.doFinal(cipherText);
 		
-		byte[] plainText = cipher.doFinal(DatatypeConverter.parseHexBinary("20070030ad5f170a973b2e60f81185282423182c1d2db1d382eff02dea0f8488"));
+//		System.out.println(Utils.toHex(DatatypeConverter.parseHexBinary("440619ee3a8c29d64380f26a21c819bdf66b0c4d3d0a79a2530d19eda5c4bf4281f11c2f7774074e52a6e6e332dab62e45c51c2b62e445fb2dd3880e98d39eea3f19def09013eabef09c772759ef431720c06f20f295034f3fc81b749765ed7d1aca997ed753c84e6a0564fedf900f21")).length());
+//		byte[] plainText = cipher.doFinal(DatatypeConverter.parseHexBinary("440619ee3a8c29d64380f26a21c819bdf66b0c4d3d0a79a2530d19eda5c4bf4281f11c2f7774074e52a6e6e332dab62e45c51c2b62e445fb2dd3880e98d39eea3f19def09013eabef09c772759ef431720c06f20f295034f3fc81b749765ed7d1aca997ed753c84e6a0564fedf900f21"));
 		System.out.println("plain text : " + Utils.toHex(plainText) + " bytes: " + plainText.length);
 		System.out.println(new String(plainText));
 	}
+	
+	/**
+	 * ENCRYPT
+	 * Esteban			A3b472xAbc
+	 * Julien			x752jU5ie2
+	 * Dorette			1234567
+	 * Lucy&Charlie		x3y45uCiC451y!?36
+	 * Micha&Felix		password
+	 * Solveig			drowssap
+	 * Ruslan&Stiliyana 12c4U05:)a201i5iya2A
+	 */
 	
 	/**
 	 * Solvejg, Relana, Fabian
 	 * key: DODODELWoD3LBOOGieMoGY
 	 * plain text : 57686174206469642074686520666f782074656c6c20796f7520616761696e3f bytes: 32
 	 * What did the fox tell you again?
+	 */
+	
+	/**
+	 * group: felix, micha
+	 * key: messiahpassengerflinchzero
+	 * plain text : 5768656e204a657375732720706c616e652073756464656e6c7920776173206869742062792074757262756c656e6365732c206865206469646e277420666c696e63682e2057687920776f756c642068652c206865277320746865206d65737369616821 bytes: 100
+	 * When Jesus' plane suddenly was hit by turbulences, he didn't flinch. Why would he, he's the messiah!
 	 */
 }
